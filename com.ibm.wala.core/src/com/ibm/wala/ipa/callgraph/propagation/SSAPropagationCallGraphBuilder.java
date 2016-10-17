bafbbb9af3385af5545c24c22c0c76e64b14eff7
Michelle Wong <michelley.wong@mail.utoronto.ca>
2016-10-17 18:02:06 -0400

    Adding instantiation of receiver objects back in

--- com.ibm.wala.core/src/com/ibm/wala/ipa/callgraph/propagation/SSAPropagationCallGraphBuilder.java
+++ com.ibm.wala.core/src/com/ibm/wala/ipa/callgraph/propagation/SSAPropagationCallGraphBuilder.java
@@ -1191,6 +1191,43 @@ public abstract class SSAPropagationCallGraphBuilder extends PropagationCallGrap
             }
           });
 
+          /********************* ANDROID MODIFICATIONS *********************/
+
+          // Add instance key for receiver pointer key if none exists
+          if (!instruction.isStatic()) {
+              PointerKey pKey = pks.get(0);
+              PointsToSetVariable pointsTo = system.findOrCreatePointsToSet(pKey);
+
+              if (pointsTo.getValue() == null) {
+                  IClassHierarchy cha = getClassHierarchy();
+                  IClass receiverClass = cha.lookupClass(instruction.getDeclaredTarget().getDeclaringClass());
+
+                  if (receiverClass.isAbstract() || receiverClass.isInterface()) {
+                      Collection<IClass> subclasses = receiverClass.isAbstract() ?
+                                                          cha.getImmediateSubclasses(receiverClass) :
+                                                          cha.getImplementors(receiverClass.getReference());
+
+                      if (!subclasses.isEmpty()) {
+                          receiverClass = subclasses.iterator().next();
+                      }
+                  }
+
+                  NewSiteReference newsite = NewSiteReference.make(instruction.getProgramCounter(), receiverClass.getReference());
+                  InstanceKey iKey = getInstanceKeyForAllocation(newsite);
+
+                  if (iKey != null) {
+                      if (DEBUG) {
+                          System.err.println("Adding instance key for type" + iKey.getConcreteType());
+                      }
+
+                      system.findOrCreateIndexForInstanceKey(iKey);
+                      system.newConstraint(pKey, iKey);
+                  }
+              }
+          }
+
+          /******************* END ANDROID MODIFICATIONS *******************/
+
           DispatchOperator dispatchOperator = getBuilder().new DispatchOperator(instruction, node,
               invariantParameters, uniqueCatch, params);
           system.newSideEffect(dispatchOperator, pks.toArray(new PointerKey[pks.size()]));
